aa6a40b719687398f0961d210b80118ea994ae5b
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-05 22:22:56 +0530

    [Mukesh] Add : email samples validated

--- src/main/java/junit/user/validation/UserRegistration.java
+++ src/main/java/junit/user/validation/UserRegistration.java
@@ -1,25 +1,29 @@
 package junit.user.validation;
 
 public class UserRegistration {
-	
+
 	public boolean checkFirstName(String input) {
 		String regex = "^[A-Z][a-z]{2,}";
-		return input.matches(regex) ;
+		return input.matches(regex);
 	}
+
 	public boolean checkLastName(String input) {
 		String regex = "^[A-Z][a-z]{2,}";
-		return input.matches(regex) ;
+		return input.matches(regex);
 	}
+
 	public boolean checkEmail(String input) {
 		String regex = "^(abc)([-/./+_]?[a-z0-9]+)?[a-z0-9]*[@][a-z1-9]+[/.][a-z]{2,3}([/.]?[a-z]{2,3})?$";
-		return input.matches(regex) ;
+		return input.matches(regex);
 	}
+
 	public boolean checkMobile(String input) {
 		String regex = "^[1-9]{1,3}[ ][6-9]{1}[0-9]{9}$";
-		return input.matches(regex) ;
+		return input.matches(regex);
 	}
+
 	public boolean checkPassword(String input) {
 		String regex = "(?=.{8,})(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]*[^a-zA-Z0-9 ][a-zA-Z0-9]*";
-		return input.matches(regex) ;
+		return input.matches(regex);
 	}
 }
--- src/test/java/junit/user/validation/UserValidationTest.java
+++ src/test/java/junit/user/validation/UserValidationTest.java
@@ -3,7 +3,6 @@ package junit.user.validation;
 import org.junit.Assert;
 import org.junit.Test;
 
-
 public class UserValidationTest {
 
 	@Test
@@ -12,140 +11,140 @@ public class UserValidationTest {
 		boolean result = user.checkFirstName("Mukesh");
 		Assert.assertTrue(result);
 	}
-	
+
 	@Test
 	public void whenGiven_ShortFirstName_ShouldReturn_False() {
 		UserRegistration user = new UserRegistration();
 		boolean result = user.checkFirstName("Mu");
 		Assert.assertFalse(result);
 	}
-	
+
 	@Test
 	public void whenGiven_NotLetterFirst_ShouldReturn_False() {
 		UserRegistration user = new UserRegistration();
 		boolean result = user.checkFirstName("Mukesh*");
 		Assert.assertFalse(result);
 	}
-	
+
 	@Test
 	public void whenGiven_NotCapitalFirst_ShouldReturn_False() {
 		UserRegistration user = new UserRegistration();
 		boolean result = user.checkFirstName("mukesh");
 		Assert.assertFalse(result);
 	}
-	
+
 	@Test
 	public void whenGiven_ProperLastName_ShouldReturn_True() {
 		UserRegistration user = new UserRegistration();
 		boolean result = user.checkLastName("Ranjan");
 		Assert.assertTrue(result);
 	}
-	
+
 	@Test
 	public void whenGiven_ShortName_ShouldReturn_False() {
 		UserRegistration user = new UserRegistration();
 		boolean result = user.checkLastName("Ra");
 		Assert.assertFalse(result);
 	}
-	
+
 	@Test
 	public void whenGiven_NotLetterLast_ShouldReturn_False() {
 		UserRegistration user = new UserRegistration();
 		boolean result = user.checkLastName("ranjan789");
 		Assert.assertFalse(result);
 	}
-	
+
 	@Test
 	public void whenGiven_NotCapitalLast_ShouldReturn_False() {
 		UserRegistration user = new UserRegistration();
 		boolean result = user.checkLastName("ranjan");
 		Assert.assertFalse(result);
 	}
-	
+
 	@Test
 	public void whenGiven_ProperEmail_ShouldReturn_True() {
 		UserRegistration user = new UserRegistration();
 		boolean result = user.checkEmail("abc.100@bl.co.in");
 		Assert.assertTrue(result);
 	}
-	
+
 	@Test
 	public void whenGiven_ImProperEmail_ShouldReturn_False() {
 		UserRegistration user = new UserRegistration();
 		boolean result = user.checkEmail("abc.@gmail.com");
 		Assert.assertFalse(result);
 	}
-	
+
 	@Test
 	public void whenGiven_ProperMobile_ShouldReturn_True() {
 		UserRegistration user = new UserRegistration();
 		boolean result = user.checkMobile("91 8989898989");
 		Assert.assertTrue(result);
 	}
-	
+
 	@Test
 	public void whenGiven_LessDigits_ShouldReturn_False() {
 		UserRegistration user = new UserRegistration();
 		boolean result = user.checkMobile("91 89874");
 		Assert.assertFalse(result);
 	}
-	
+
 	@Test
 	public void whenGiven_ImproperCountryCode_ShouldReturn_False() {
 		UserRegistration user = new UserRegistration();
 		boolean result = user.checkMobile("00 8987489898");
 		Assert.assertFalse(result);
 	}
-	
+
 	@Test
 	public void whenGiven_WithoutCountryCode_ShouldReturn_False() {
 		UserRegistration user = new UserRegistration();
 		boolean result = user.checkMobile("8987489898");
 		Assert.assertFalse(result);
 	}
-	
+
 	@Test
 	public void whenGiven_MoreThanTenDigits_ShouldReturn_False() {
 		UserRegistration user = new UserRegistration();
 		boolean result = user.checkMobile("91 89898989889");
 		Assert.assertFalse(result);
 	}
-	
+
 	@Test
 	public void whenGiven_ProperPassword_ShouldReturn_True() {
 		UserRegistration user = new UserRegistration();
 		boolean result = user.checkPassword("India@2020");
 		Assert.assertTrue(result);
 	}
-	
+
 	@Test
 	public void whenGiven_LessThanEightDigits_ShouldReturn_False() {
 		UserRegistration user = new UserRegistration();
 		boolean result = user.checkPassword("India@6");
 		Assert.assertFalse(result);
 	}
-	
+
 	@Test
 	public void whenGiven_WithoutCapitalLetter_ShouldReturn_False() {
 		UserRegistration user = new UserRegistration();
 		boolean result = user.checkPassword("india@2020");
 		Assert.assertFalse(result);
 	}
-	
+
 	@Test
 	public void whenGiven_WithoutNumber_ShouldReturn_False() {
 		UserRegistration user = new UserRegistration();
 		boolean result = user.checkPassword("India@oooo");
 		Assert.assertFalse(result);
 	}
-	
+
 	@Test
 	public void whenGiven_WithoutSpecialCharater_ShouldReturn_False() {
 		UserRegistration user = new UserRegistration();
 		boolean result = user.checkPassword("India2020");
 		Assert.assertFalse(result);
 	}
-	
+
 	@Test
 	public void whenGiven_MoreThanOneSpecialCharater_ShouldReturn_False() {
 		UserRegistration user = new UserRegistration();
