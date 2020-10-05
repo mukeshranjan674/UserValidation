e4cf10d84097e817432af2e5fb9598f2d9b23e8d
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-05 18:02:37 +0530

    [Mukesh] update : first name check

--- src/main/java/junit/user/validation/UserRegistration.java
+++ src/main/java/junit/user/validation/UserRegistration.java
@@ -1,15 +1,28 @@
 package junit.user.validation;
 
+import java.util.Scanner;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
-
 public class UserRegistration {
 	
-	private static final Logger log= LogManager.getLogger(App.class);
+	private static final Logger LOG= LogManager.getLogger(UserRegistration.class);
+	Scanner sc = new Scanner(System.in);
+	
+	public boolean checkFirstName() {
+
+		LOG.info("\nEnter First Name\n");
+		String input = sc.next();
+		String regex = "^[A-Z][a-z]{2,}";
+		return input.matches(regex) ;
+	}
+		
+	
 	public static void main(String[] args) {
-		log.info("\n***** Welcome to User Registration System *****");
+		LOG.info("\n***** Welcome to User Registration System *****");
+		UserRegistration user = new UserRegistration();
+		LOG.info("\n" + user.checkFirstName());
 	}
 
 }
