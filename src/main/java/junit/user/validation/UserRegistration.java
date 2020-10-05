231b16d246a00bcd215390960b15a43c6cfa4686
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-05 16:58:15 +0530

    [Mukesh] add : User registration welcome screen

---
+++ src/main/java/junit/user/validation/UserRegistration.java
@@ -0,0 +1,15 @@
+package junit.user.validation;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+
+public class UserRegistration {
+	
+	private static final Logger log= LogManager.getLogger(App.class);
+	public static void main(String[] args) {
+		log.info("\n***** Welcome to User Registration System *****");
+	}
+
+}
